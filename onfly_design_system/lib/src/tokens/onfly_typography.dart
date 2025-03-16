35c08a37b0d0dc320f8e8e7b69bd7f776280e53c
limadlp <dlplima@hotmail.com>
2025-03-16 20:39:39 -0300

    chore: add google fonts

--- onfly_design_system/lib/src/tokens/onfly_typography.dart
+++ onfly_design_system/lib/src/tokens/onfly_typography.dart
@@ -1,79 +1,71 @@
 import 'package:flutter/material.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+
 class OnflyTypography {
   // Headings
-  static const TextStyle titleXL = TextStyle(
+  static TextStyle get titleXL => GoogleFonts.poppins(
     fontSize: 24,
     fontWeight: FontWeight.bold,
-    fontFamily: 'Poppins',
     height: 1.3,
   );
 
-  static const TextStyle titleLG = TextStyle(
+  static TextStyle get titleLG => GoogleFonts.poppins(
     fontSize: 20,
     fontWeight: FontWeight.bold,
-    fontFamily: 'Poppins',
     height: 1.3,
   );
 
-  static const TextStyle titleMD = TextStyle(
+  static TextStyle get titleMD => GoogleFonts.poppins(
     fontSize: 16,
     fontWeight: FontWeight.bold,
-    fontFamily: 'Poppins',
     height: 1.3,
   );
 
-  static const TextStyle titleSM = TextStyle(
+  static TextStyle get titleSM => GoogleFonts.poppins(
     fontSize: 14,
     fontWeight: FontWeight.bold,
-    fontFamily: 'Poppins',
     height: 1.3,
   );
 
   // Body Text
-  static const TextStyle bodyLG = TextStyle(
+  static TextStyle get bodyLG => GoogleFonts.poppins(
     fontSize: 14,
     fontWeight: FontWeight.normal,
-    fontFamily: 'Poppins',
     height: 1.5,
   );
 
-  static const TextStyle bodyMD = TextStyle(
+  static TextStyle get bodyMD => GoogleFonts.poppins(
     fontSize: 12,
     fontWeight: FontWeight.normal,
-    fontFamily: 'Poppins',
     height: 1.5,
   );
 
-  static const TextStyle bodySM = TextStyle(
+  static TextStyle get bodySM => GoogleFonts.poppins(
     fontSize: 10,
     fontWeight: FontWeight.normal,
-    fontFamily: 'Poppins',
     height: 1.5,
   );
 
   // Subtitles
-  static const TextStyle subtitleMD = TextStyle(
+  static TextStyle get subtitleMD => GoogleFonts.poppins(
     fontSize: 12,
     fontWeight: FontWeight.w500,
-    fontFamily: 'Poppins',
     height: 1.5,
     letterSpacing: 0.2,
   );
 
-  static const TextStyle subtitleSM = TextStyle(
+  static TextStyle get subtitleSM => GoogleFonts.poppins(
     fontSize: 10,
     fontWeight: FontWeight.w500,
-    fontFamily: 'Poppins',
     height: 1.5,
     letterSpacing: 0.2,
   );
 
   // Labels
-  static const TextStyle label = TextStyle(
+  static TextStyle get label => GoogleFonts.poppins(
     fontSize: 12,
     fontWeight: FontWeight.w500,
-    fontFamily: 'Poppins',
     height: 1.3,
   );
 }
