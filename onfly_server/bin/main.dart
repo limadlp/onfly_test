d6f08c0df3dbfc96a7a30896c8f8db67618e1231
limadlp <dlplima@hotmail.com>
2025-03-17 17:35:15 -0300

    chore: allow cors

--- onfly_server/bin/main.dart
+++ onfly_server/bin/main.dart
@@ -2,10 +2,12 @@ import 'dart:io';
 import 'package:onfly_api/routes.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 
 void main() async {
   // Set up middleware and routing
   final handler = const Pipeline()
+      .addMiddleware(corsHeaders())
       .addMiddleware(logRequests()) // Logs all incoming requests
       .addHandler(setupRoutes().call); // Handles routing
 
